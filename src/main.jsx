59a573c28ba78c5d880b2e92709f43fc0814046a
CryseXIII <viktorpagels@aol.com>
2026-01-10 13:49:59 +0100

    feat(routing): configure React Router and integrate Button navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import HelloWorld from './pages/HelloWorld.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/hello" element={<HelloWorld />} />
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>,
 )
